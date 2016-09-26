8904cbec327359188b37b64e52ff87a31b5f3c2d
derivativetaste <cmatthie@smu.edu>
2016-09-26 16:57:09 -0500

    adding Lab 3

---
+++ Lab3 - SQLite/L3I_Matthie_SQLitelab/L3I_Matthie_SQLitelabTests/L3I_Matthie_SQLitelabTests.swift
@@ -0,0 +1,36 @@
+//
+//  L3I_Matthie_SQLitelabTests.swift
+//  L3I_Matthie_SQLitelabTests
+//
+//  Created by devbot on 9/26/16.
+//  Copyright © 2016 devbot. All rights reserved.
+//
+
+import XCTest
+@testable import L3I_Matthie_SQLitelab
+
+class L3I_Matthie_SQLitelabTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
